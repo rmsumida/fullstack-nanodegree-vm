df3f474b1b6d6ce89b26e402804082ef183debc0
rmsumida <ryan.sumida@gmail.com>
2016-06-09 20:11:08 -0700

    Add files via upload

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,7 +4,7 @@
 #
 
 import psycopg2
-
+import bleach
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -13,14 +13,34 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM matches")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM players")
+    conn.commit()
+    conn.close()
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    #  SQL command to count the number of ids from the players table, returned as an integer
+    c.execute("SELECT count(players.id)::int AS num FROM players")
+    count = c.fetchall()
+    for row in count:
+        count = row[0]
+    conn.close()
+    #print "print player count"
+    #print count
+    return count
 
 
 def registerPlayer(name):
@@ -32,6 +52,15 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    # Sanitize strings used in the 'name' variable before inserting it into the database
+    name = bleach.clean(name)
+    # SQL command to insert the variable 'name' into the players table.
+    # The player id will be autoincrement to the next integer when the row is inserted
+    c.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,7 +76,14 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
+    conn = connect()
+    c = conn.cursor()
+    # SQL command to return a list of player ids, names, wins and number of matches played
+    # The matches_played view will be joined to get the total number of matches played for each player id
+    c.execute("SELECT wins.id, wins.name, wins.wins, matches_played.played as matches FROM wins LEFT JOIN matches_played ON wins.id = matches_played.id ORDER BY wins DESC")
+    standings = c.fetchall()
+    conn.close()
+    return standings
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,7 +92,15 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
+    # Sanitize strings used in the 'winner' and 'loser' variable before inserting it into the database
+    winner = bleach.clean(winner)
+    loser = bleach.clean(loser)
+    conn = connect()
+    c = conn.cursor()
+    # SQL command to record the match winner and loser into the matches table
+    c.execute("INSERT INTO matches (id_winner, id_loser) VALUES (%s, %s)", (winner, loser))
+    conn.commit()
+    conn.close()
  
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
@@ -73,5 +117,31 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    conn = connect()
+    c = conn.cursor()
+    # Get table with id and name sorted by total wins in descending order
+    c.execute("SELECT id, name FROM wins ORDER BY wins DESC")
+    standings = c.fetchall()
+    # print 'Print standings'
+    # print standings
+    num_players = countPlayers()
+    # print 'Print Number of Players'
+    # print num_players
+    player_list = [player[0:2] for player in standings]
+    # print 'Print Player List'
+    # print player_list
+    pairings = []
+    i = 0
+    while i < num_players:
+        # Create list of tuples, each with a pair of players
+        match_pair = player_list[i] + player_list[i + 1]
+        # Append pair tuple to the pairings list
+        pairings.append(match_pair)
+        # Jump to first player in next pair
+        i += 2
+    # print 'Print Pairings'
+    # print pairings
+    return pairings
+    conn.close()
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,38 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+-- Table with list of unique player ids and player names
 
+-- If exists, delete the tournament database
+DROP DATABASE IF EXISTS tournament;
 
+-- Create the tournament database
+CREATE DATABASE tournament;
+
+-- Connect to the tournament database
+\c tournament
+
+CREATE TABLE players ( id SERIAL PRIMARY KEY, name TEXT);
+
+-- table with list of unique match ids, the id of the winning player and the id of the losing player
+CREATE TABLE matches ( id_match SERIAL PRIMARY KEY,
+						id_winner INTEGER REFERENCES players(id),
+						id_loser INTEGER REFERENCES players(id)
+						);
+
+--- View that aggregates the number matches played and groups the count by player id
+CREATE VIEW matches_played AS SELECT players.id,
+						players.name,
+						COUNT(matches.id_match)::int AS played
+						from players LEFT JOIN matches
+						on players.id = matches.id_winner
+						or players.id = matches.id_loser
+						GROUP BY players.id;
+
+-- View that counts the number of wins a player has in the matches table and groups the count by player id
+CREATE VIEW wins AS SELECT players.id::int,
+						players.name,
+						COUNT(matches.id_winner)::int AS wins
+						FROM players LEFT JOIN matches
+						ON players.id = matches.id_winner
+						GROUP BY players.id;
